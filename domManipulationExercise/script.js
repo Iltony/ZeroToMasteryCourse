4bb2d025744ae4cc2e6203ffa1944ba3d8f9c445
Antonio Rodriguez <antorodriguez@gmail.com>
2019-05-05 22:24:54 -0300

    complete exercise with done on click

--- domManipulationExercise/script.js
+++ domManipulationExercise/script.js
@@ -1,6 +1,7 @@
 var button = document.getElementById("enter")
 var input = document.getElementById("userInput")
 var ul = document.querySelector("ul")
+var listItem = document.querySelectorAll("li")
 
 function createListElement(){
 	var li= document.createElement("li");
@@ -9,19 +10,25 @@ function createListElement(){
 		input.value = ""
 }
 
-function addListAft	  	erClick() {
+function addListAfterClick() {
 	if (input.value.length > 0){
 		createListElement()
 	}
 }
 
 function addListAfterEnter(event) {
-	if (input.value.length > 0 and event.keycode === 13){
+	if (input.value.length > 0 && event.keycode === 13){
 		createListElement()
 	}
+}
+
+function toggleDoneElement(event) {
+	event.target.classList.toggle("done");
 }
 
 
 button.addEventListener("click", addListAfterClick)
 
 input.addEventListener("keypress", addListAfterEnter)
+
+listItem.forEach(x=> x.addEventListener("click", toggleDoneElement))
